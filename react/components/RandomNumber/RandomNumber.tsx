1e154347a7bbb34c77a59fa31dfcc4ced4ec3896
JONATHAN AGUILERA <jhonatan@jumpdigital.co>
2020-12-03 15:26:01 -0500

    Ajustes header desktop

--- react/components/RandomNumber/RandomNumber.tsx
+++ react/components/RandomNumber/RandomNumber.tsx
@@ -4,9 +4,15 @@ import styles from './styles.css';
 const RandomNumber = () => {
 
     return (
-        <h2 className={ `vtex-rich-text-0-x-paragraph--customjd ${styles.textRandomNumber}` }>
-            Este es mi mensaje 3
-        </h2>
+        <>
+            <h2 className={ `vtex-rich-text-0-x-paragraph--randomNumber ${styles.textRandomNumber}`}>
+                Prueba
+            </h2>
+            <p className={ `vtex-rich-text-0-x-paragraph--randomNumber ${styles.textRandomNumber}` }>
+                No lo pierdas!!! tenemos 17 personas insteresadas en este producto
+            </p>
+        </>
+        
 
     )
 }
